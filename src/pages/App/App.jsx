d7bab6a55feeef1d7ef6876d90901e105f24fc1f
janicavc <crisostomo_janica@yahoo.com>
2023-08-15 23:23:31 -0700

    update navbar in app

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,24 +1,29 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { movies } from '../../data.js';
 import './App.css';
 import MovieDetailPage from '../MovieDetailPage/MovieDetailPage';
 import MoviesListPage from '../MoviesListPage/MoviesListPage';
 import LoginPage from '../LoginPage/LoginPage';
 import ActorListPage from '../ActorListPage/ActorListPage';
+import NavBar from '../../components/NavBar/NavBar.jsx';
 
 export default function App() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   return (
     <main className="App">
       { user ?
-        <Routes>
-          <Route path='/' element={<MoviesListPage />}></Route>
-          <Route path='/movies/:movieName' element={<MovieDetailPage />}></Route>
-          <Route path='/actors' element={<ActorListPage />}></Route>
-        </Routes>
+        <>
+          <NavBar user={user} />
+          <Routes>
+            <Route path='/' element={<MoviesListPage />}></Route>
+            <Route path='/movies/:movieName' element={<MovieDetailPage />}></Route>
+            <Route path='/actors' element={<ActorListPage />}></Route>
+          </Routes>
+        </>
         :
-        <MovieDetailPage/>
+        <LoginPage signup={signup} />
       }
     </main>
   );
